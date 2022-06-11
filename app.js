9da1cb5e9e28dd4344a53a1c069b4134f7b673e3
Bongani <mkhumbie34@gmail.com>
2022-06-11 13:38:32 +0200

    mobile virsion completed

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const video_container = document.querySelector(`.video_container`)
 const words = document.querySelector(`.words`)
 const controls = document.querySelector(`.controls`)
 const btns = controls.querySelectorAll(`.icon`)
+const left = document.querySelector(`.left`)
+const right = document.querySelector(`.right`)
+const link_btn = document.querySelector(`.started`)
+const close_btn = document.querySelector(`.close`)
+let bubble = document.querySelector(`.bubble`)
 
 video.muted = true
 
+left.classList.add(`none`)
+right.classList.add(`none`)
 video.addEventListener(`click`,()=>{
     hide()
     control()
@@ -13,30 +20,45 @@ video.addEventListener(`click`,()=>{
     refresh()
 })
 
-
+close_btn.addEventListener(`click`,()=>{
+    bubble.classList.add(`none`)
+})
 function hide (){
     //removes the cliph path
     video.classList.remove(`video`)
     video.classList.add(`open`)
-    video.classList.add(`video_open`)
-    
+    //remove position down
+    video_container.classList.remove(`down`)
+    //remove grid
+    video_container.classList.remove(`divide`)
     //show controls
-    controls.classList.remove(`hide`)
+    controls.classList.remove(`none`)
+    link_btn.classList.remove(`none`)
     //hide prompt
-     words.classList.add(`hide`)
-
+    words.classList.add(`hide`)
+    left.classList.remove(`none`)
+    right.classList.remove(`none`)
+    //close btn
+    close_btn.classList.add(`none`)
 }
 function close(){
      //removes the cliph path
     video.classList.add(`video`)
     video.classList.remove(`open`)
-    video.classList.remove(`video_open`)
-    
-    //show controls
-    controls.classList.add(`hide`)
+   //add position down
+    video_container.classList.add(`down`)
+    //add grid
+    video_container.classList.add(`divide`)
+    //close controls
+    controls.classList.add(`none`)
+    left.classList.add(`none`)
+    right.classList.add(`none`)
     //hide prompt
-     words.classList.remove(`hide`)
-
+    words.classList.remove(`hide`)
+    link_btn.classList.add(`none`)
+     //close btn
+    close_btn.classList.remove(`none`)
+  
 }
 function refresh (){
     video.currentTime = 0
